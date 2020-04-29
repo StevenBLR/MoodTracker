bfd36ee44554049dc21dc186237454ceb4a204f7
@Sbelair <steven.belair@on-x.com>
2020-04-30 00:08:58 +0200

    Revert "Merge branch 'ViewPager2' of https://github.com/StevenBLR/MoodTracker into ViewPager2"
    This reverts commit f71b887aa9c5f0f2f076c2d7bb14364fc138d897, reversing
    changes made to 504f8d1ae79e482983e4fe4fa29cd615df210450.

--- app/src/main/java/com/steven/blr/moodtracker/MainActivity.java
+++ app/src/main/java/com/steven/blr/moodtracker/MainActivity.java
@@ -1,28 +1,18 @@
 package com.steven.blr.moodtracker;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-import android.util.Log;
-
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends FragmentActivity
+public class MainActivity extends AppCompatActivity
 {
-
-    private ViewPager2 pager;
-    private FragmentStateAdapter pagerAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_screen_slide);
 
         //3 - Configure ViewPager
         this.configureViewPager();
@@ -30,21 +20,8 @@ public class MainActivity extends FragmentActivity
 
     private void configureViewPager(){
         // 1 - Get ViewPager from layout
-        pager = findViewById(R.id.activity_main_viewpager);
-        pagerAdapter = new ScreenSlidePageAdapter(this, getResources().getIntArray(R.array.colorPagesViewPager));
-        Log.e("TAG", pager.toString());
-        pager.setAdapter(pagerAdapter);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (pager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            pager.setCurrentItem(pager.getCurrentItem() - 1);
-        }
+        ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
+        // 2 - Set Adapter PageAdapter and glue it together
+        pager.setAdapter(new ScreenSlidePagerActivity(getResources().getIntArray(R.array.colorPagesViewPager)).getPageAdapter());
     }
 }
